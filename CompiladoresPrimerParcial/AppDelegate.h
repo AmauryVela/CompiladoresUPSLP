d7991a51f6a7dfecf26efa0db56dbaf387002116
MauVelaM <MauVela@imac-de-mau.lan>
2015-02-23 22:07:51 -0600

    Initial Commit

---
+++ CompiladoresPrimerParcial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CompiladoresPrimerParcial
+//
+//  Created by Mau Vela on 23/02/15.
+//  Copyright (c) 2015 Mau Vela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CompiladoresPrimerParcial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CompiladoresPrimerParcial
+//
+//  Created by Mau Vela on 23/02/15.
+//  Copyright (c) 2015 Mau Vela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
