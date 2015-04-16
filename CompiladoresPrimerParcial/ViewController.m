aeda391bc18884bc497fe3a890bb1aa891382c91
Amaury Vela <amauryvela@Macbook.local>
2015-04-16 15:44:56 -0500

    -Normailza con punto

--- CompiladoresPrimerParcial/ViewController.m
+++ CompiladoresPrimerParcial/ViewController.m
@@ -61,10 +61,164 @@
 {
     [[self view] endEditing:YES];
 }
+-(NSString*)checkStringAndAdd{
+    
+    NSArray * tokens = [self tokenize: expresion.text];
+    for (NSString *token in tokens){
+         if ([token compare: @"("] ==0){
+            // push opening brackets on the stack, will be dismissed later
+        }
+        NSLog(@"Es: %@",token);
+    }
+    
+    
+    NSMutableArray *letterArray = [NSMutableArray array];
+    NSString *letters = expresion.text;
+    [letters enumerateSubstringsInRange:NSMakeRange(0, [letters length])
+                                options:(NSStringEnumerationByComposedCharacterSequences)
+                             usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                                 [letterArray addObject:substring];
+                             }];
+    NSString*test=@"";
+   int x=0;
+    for (NSString *i in letterArray){
+        if (x<letterArray.count-1) {
+            NSString*sig=[letterArray objectAtIndex:x+1];
+            NSString*ptemp=test;
 
+            
+            
+            
+            
+            if (![[letterArray objectAtIndex:x] isEqualToString:@"*"]) {
+                if (![[letterArray objectAtIndex:x] isEqualToString:@"?"]) {
+                    if (![[letterArray objectAtIndex:x] isEqualToString:@"|"]) {
+                        if (![[letterArray objectAtIndex:x] isEqualToString:@"+"]) {
+                            test=[NSString stringWithFormat:@"%@%@",ptemp,[letterArray objectAtIndex:x]];
+                            NSLog(@"%@ y %d elquesigue %@ cadena %@",i,x,sig,test);
+                            if (![sig isEqualToString:@"*"]) {
+                                if (![sig isEqualToString:@"?"]) {
+                                    if (![sig isEqualToString:@"|"]) {
+                                        if (![sig isEqualToString:@"+"]) {
+                                            NSString*ptempd=test;
+                                            if ([sig isEqualToString:@" "]) {
+                                                
+
+                                            }else{
+                                                test=[NSString stringWithFormat:@"%@.",ptempd];
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }else{
+                            test=[NSString stringWithFormat:@"%@%@",ptemp,[letterArray objectAtIndex:x]];
+                        }
+                    }
+                    else{
+                        test=[NSString stringWithFormat:@"%@%@",ptemp,[letterArray objectAtIndex:x]];
+
+                        
+                    }
+                }else{
+                    test=[NSString stringWithFormat:@"%@%@",ptemp,[letterArray objectAtIndex:x]];
+                }
+            }else{
+                test=[NSString stringWithFormat:@"%@%@",ptemp,[letterArray objectAtIndex:x]];
+            }
+            
+            
+            
+                    }
+       
+        x++;
+    }
+    NSLog(@"*********%@*******",test);
+    return test;
+
+}
+-(IBAction)convertorPun:(id)sender{
+    expresion.text=[expresion.text uppercaseString];
+    if ( ! [self esExpBalanceada:expresion.text]){
+        NSLog(@"Expresion no balanceada, revice sus ""("" y "")""");
+        resultado.text=@"Expresion no balanceada, revice sus (  )";
+    }else{
+        /*unichar c;
+         
+         NSMutableArray * caracteres = [NSMutableArray arrayWithCapacity:[expresion.text length]];
+         for (int i=0; i<caracteres.count; i++) {
+         c = [expresion.text characterAtIndex: i];
+         
+         switch (c) {
+         case <#constant#>:
+         <#statements#>
+         break;
+         
+         default:
+         break;
+         }
+         }*/
+        SimpleStack * opStack = [[SimpleStack alloc] init];
+        NSMutableString * output = [NSMutableString stringWithCapacity:[expresion.text length]];
+        
+        [opStack print];
+        
+        NSArray * tokens = [self tokenize: expresion.text];
+        for (NSString *token in tokens){
+            if ([self precedenceOf:token] != 0){
+                // token is an operator, pop all operators of higher or equal precedence off the stack, and append them to the output
+                NSString *op = [opStack peek];
+                while (op && [self precedenceOf:op] != 0 &&
+                       [self precedenceOf: op isHigherOrEqualThan: token]) {
+                    [output appendString: [NSString stringWithFormat: @"%@ ", [opStack pop]]];
+                    op = [opStack peek];
+                }
+                // then push the operator on the stack
+                [opStack push:token];
+                
+                [opStack print];
+                
+            } else if ([token compare: @"("] ==0){
+                // push opening brackets on the stack, will be dismissed later
+                [opStack push:token];
+            } else if ([token compare: @")"] ==0) {
+                // closing bracket :
+                // pop operators off the stack and append them to the output while the popped element is not the opening bracket
+                NSString  * op = [opStack pop];
+                while ( op  && ([op compare: @"("] != 0)){
+                    [output appendString: [NSString stringWithFormat: @"%@ ", op]];
+                    op = [opStack pop];
+                }
+                if ( ! op || ([op compare: @"("]  != 0)){
+                    NSLog(@"Error : unbalanced brackets in expression");
+                }
+            } else {
+                //token is an operand, append it to the output
+                [output appendString: [NSString stringWithFormat: @"%@ ", token]];
+            }
+            
+            [opStack print];
+            
+        }
+        
+        //pop remaining operators off the stack, and append them to the output
+        while (! [opStack empty]) {
+            [output appendString: [NSString stringWithFormat: @"%@ ", [opStack pop]]];
+        }
+        
+        
+        resultado.text=[NSString stringWithFormat:@"%@", [output stringByTrimmingCharactersInSet:
+                                                          [NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+        NSDictionary*tmp=@{@"Inf":expresion.text,@"pos":resultado.text};
+        [self guardarHistorial:tmp];
+    }
+
+}
 -(IBAction)convertir:(id)sender{
     expresion.text=[expresion.text uppercaseString];
-    
+     expresion.text=[self checkStringAndAdd];
     if ( ! [self esExpBalanceada:expresion.text]){
         NSLog(@"Expresion no balanceada, revice sus ""("" y "")""");
         resultado.text=@"Expresion no balanceada, revice sus (  )";
@@ -170,10 +324,13 @@
     
     for (int i = 0; i< length; i++){
         c = [expression characterAtIndex: i];
+        NSLog(@"Tiene %c ",c);
+
         switch (c) {
             case '+':
             case '|':
             case '*':
+                case '.':
             case '^':
                 nextMinusSignIsNegativeOperator = YES;
                 [self addNumber: numberBuf andToken: c toTokens:tokens];
@@ -212,7 +369,6 @@
             case 'Q':
             case 'R':
             case '?':
-            case '.':
 
             case 'd':
                 nextMinusSignIsNegativeOperator = NO;
@@ -246,8 +402,9 @@
 - (NSUInteger) precedenceOf: (NSString*) operator{
     if ([operator compare: @"-"] == 0 )
         return 1;
-    else if ([operator compare: @"."] == 0 )
-        return 1;
+    else if ([operator compare: @"."] == 0 ){
+        NSLog(@"Tiene . ");
+        return 2;}
     else if ([operator compare: @"?"] == 0 )
        return 2;
     else if ([operator compare: @"|"] == 0 )
